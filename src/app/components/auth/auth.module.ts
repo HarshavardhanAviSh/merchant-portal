6a8c1568f0431d2f10f9507ddacda04daa4a0cf8
HarshavardhanAviSh <harshavardhan.gaikwad@npstx.com>
2023-08-16 11:57:19 +0530

    auth module added

---
+++ src/app/components/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiModule } from 'src/app/ui/ui.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UiModule
+  ]
+})
+export class AuthModule { }
